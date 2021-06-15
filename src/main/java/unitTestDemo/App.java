0501df6cda6148bbe1333fee507dfe2861847ee2
Myra T <myra.tso664@gmail.com>
2021-06-14 23:37:21 -0700

    Project inital commit

---
+++ src/main/java/unitTestDemo/App.java
@@ -0,0 +1,13 @@
+package unitTestDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
